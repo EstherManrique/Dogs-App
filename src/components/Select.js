7970763c9d922946547f31ace9e064a164be0e64
EstherManrique <esmango0610@gmail.com>
2021-11-23 20:07:39 -0600

    Added Error component

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import getBreeds from "../helpers/getBreeds";
+import Error from "./Error";
 
 const initialBreeds = [
   {
@@ -14,6 +15,7 @@ const initialBreeds = [
 
 const Select = ({ updateDog }) => {
   const [breeds, setBreeds] = useState(initialBreeds);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     updateBreeds();
@@ -21,20 +23,28 @@ const Select = ({ updateDog }) => {
 
   const updateBreeds = () => {
     getBreeds()
-    .then((newBreeds) => {
-      setBreeds(newBreeds);
-
-    })
-  }
+      .then((newBreeds) => {
+        setBreeds(newBreeds);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError("Error al cargar las razas");
+      });
+  };
 
   return (
-    <select onChange={(e) => updateDog(e.target.value)}>
-      {breeds.map((breed) => (
-        <option value={breed.id} key={breed.id}>
-          {breed.name}
-        </option>
-      ))}
-    </select>
+    <>
+      <select onChange={(e) => updateDog(e.target.value)}>
+        {breeds.map((breed) => (
+          <option value={breed.id} key={breed.id}>
+            {breed.name}
+          </option>
+        ))}
+      </select>
+
+      { error && <Error error={error} />}
+      
+    </>
   );
 };
 
--- src/helpers/getBreeds.js
+++ src/helpers/getBreeds.js
@@ -1,6 +1,12 @@
 const getBreeds = async () => {
   const url = "https://api.thedogapi.com/v1/breeds";
   const res = await fetch(url);
+
+  if(!res.ok) {
+    const { url, status, statusTex } = res;
+    throw Error(`Error ${status} ${statusTex} in fetch ${url}`);
+  }
+
   const breeds = await res.json();
 
   return breeds;
